3893563499291e013d511ae941d3d2c7dd57d970
Kira 🤍 <alexanderpodshibiakin@yandex.ru>
2025-12-04 00:59:17 +0300

    Добавление RU локализации в коде и именах (#13)
    * [HOTFIX] FIX LIGHT MELEE LOGS (#41655)
    
    God fugcking
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
    
    * fix?
    
    * добавление RU локализации в коде и именах
    
    * add prototype names
    
    * правка имён вульп
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>
    Co-authored-by: kira-murs404 <kira-murs404@users.noreply.github.com>

--- Content.Server/GameTicking/GameTicker.Spawning.cs
+++ Content.Server/GameTicking/GameTicker.Spawning.cs
@@ -284,6 +284,7 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement-special",
                             ("character", MetaData(mob).EntityName),
+                            ("gender", character.Gender), // Corvax-LastnameGender
                             ("entity", mob),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
@@ -295,6 +296,7 @@ namespace Content.Server.GameTicking
                     _chatSystem.DispatchStationAnnouncement(station,
                         Loc.GetString("latejoin-arrival-announcement",
                             ("character", MetaData(mob).EntityName),
+                            ("gender", character.Gender), // Corvax-LastnameGender
                             ("entity", mob),
                             ("job", CultureInfo.CurrentCulture.TextInfo.ToTitleCase(jobName))),
                         Loc.GetString("latejoin-arrival-sender"),
